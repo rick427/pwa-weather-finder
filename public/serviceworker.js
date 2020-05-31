ca1c8dfa8a7d60908b818527740e7e3a4632e0d9
rick427 <rchardnjoku@yahoo.com>
2020-05-31 14:13:10 +0100

    Rounding up

---
+++ public/serviceworker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "version-1";
+const urlsToCache = ['index.html', 'offline.html'];
+
+// where this = the service worker;
+const self = this;
+
+// Install service worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('cache opened');
+
+            return cache.addAll(urlsToCache);
+        })
+    )
+})
+
+// Listen for requests
+self.addEventListener('fetch', event => {
+    // respond with something when a fetch request is triggered
+    event.respondWith(
+        caches.match(event.request).then(() => {
+            return fetch(event.request).catch(() => caches.match('offline.html'))
+        })
+    )
+})
+
+// Activate the service worker
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map(cacheName => {
+                if(!cacheWhitelist.includes(cacheName)){
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+    )
+})
